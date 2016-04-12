4cfc9b17eb0a31394f40e48b876d91f428cc8740
Glicknork <glicknork@hotmail.com>
2016-04-12 17:15:41 +0100

    Intelligent block spawning implemented
    Raycasts detect if a blank box is present to determine what to spawn

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,9 +4,17 @@ using System.Collections;
 public class BallController : MonoBehaviour
 {
 
-    public float jumpVelocity = 10f;
+    public float gravity = 20f;
+
+    public float smallJump = 20f;
+
+    public float bigJump = 30f;
+
+    bool grounded;
 
     Rigidbody body;
+
+    Collider contactDetector;
 
     // Use this for initialization
     void Start()
@@ -20,20 +28,65 @@ public class BallController : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (grounded && Input.GetKeyDown(KeyCode.UpArrow))
         {
-            body.velocity = new Vector3(0f, jumpVelocity, body.velocity.z);
+            if (Physics.gravity.y < 0f)
+            {
+                body.velocity = new Vector3(0f, smallJump, body.velocity.z);
+            }
+            if (Physics.gravity.y > 0f)
+            {
+                body.velocity = new Vector3(0f, -smallJump, body.velocity.z);
+            }
+        }
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Physics.gravity.y < 0f)
+            {
+                body.velocity = new Vector3(0f, bigJump, body.velocity.z);
+            }
+            if (Physics.gravity.y > 0f)
+            {
+                body.velocity = new Vector3(0f, -bigJump, body.velocity.z);
+            }
         }
     }
 
 
 
-    void OnTriggerEnter(Collider coll)
+    void OnTriggerExit(Collider coll)
     {
+        if(coll.gameObject.tag == "Block")
+        {
+            grounded = false;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider coll)
+    {        
         if (coll.gameObject.tag == "Death")
         {
             Destroy(gameObject);
+        }        
+    }
+
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.tag == "Block")
+        {
+            grounded = true;
         }
+
+        if (coll.gameObject.tag == "GravZoneTop")
+        {
+            Physics.gravity = new Vector3 (0.0f, gravity, 0.0f);
+        }
+        if (coll.gameObject.tag == "GravZoneBottom")
+        {
+            Physics.gravity = new Vector3(0.0f, -gravity, 0.0f);
+        }        
     }
 }
 
--- Assets/Scripts/SpawnNewBlock.cs
+++ Assets/Scripts/SpawnNewBlock.cs
@@ -3,24 +3,49 @@ using System.Collections;
 
 public class SpawnNewBlock : MonoBehaviour {
 
-    public GameObject[] blocks; 
+    public GameObject[] blocks;
+
+    public GameObject blankBlock; 
     
     public Transform upperBlockSpawn;
     public Transform lowerBlockSpawn;
 
+    public Transform spawnCheckerUpper;
+    public Transform spawnCheckerLower;
+        
+
+    int layerMask = 1 << 8;
+        
+
 	void OnTriggerEnter(Collider coll)
     {
         if(coll.gameObject.tag == "Block")
-        {            
-            Instantiate(ReturnNewSpawnBlock(), upperBlockSpawn.position, upperBlockSpawn.rotation);
-            Instantiate(ReturnNewSpawnBlock(), lowerBlockSpawn.position, lowerBlockSpawn.rotation);
+        {
+            // instanatiates an upperblock clone at the spawn spot and rotates it so it's pointing down
+            GameObject upperBlockClone = (GameObject) Instantiate(ReturnNewSpawnBlock(), upperBlockSpawn.position, upperBlockSpawn.rotation);
+            Vector3 rotationVector = upperBlockClone.transform.rotation.eulerAngles;
+            rotationVector.x = 90;
+            upperBlockClone.transform.rotation = Quaternion.Euler(rotationVector);            
+            // instantiates a lowerblock clone at the spawn spot
+            GameObject lowerBlockClonce = (GameObject)Instantiate(ReturnNewSpawnBlock(), lowerBlockSpawn.position, lowerBlockSpawn.rotation);
         }        
        
     }
 
     GameObject ReturnNewSpawnBlock()
     {
-        return blocks[Random.Range(0, blocks.Length)];
+        if (Physics.Raycast(spawnCheckerLower.position, lowerBlockSpawn.position, Mathf.Infinity, layerMask, QueryTriggerInteraction.Collide) &&
+            Physics.Raycast(spawnCheckerUpper.position, upperBlockSpawn.position, Mathf.Infinity, layerMask, QueryTriggerInteraction.Collide))
+        {
+            Debug.Log("blank FOUND");
+            return blocks[Random.Range(0, blocks.Length)];
+        }
+        else
+        {
+            Debug.Log("blank not found");
+            return blankBlock;
+        }
+        
     }
 
 
